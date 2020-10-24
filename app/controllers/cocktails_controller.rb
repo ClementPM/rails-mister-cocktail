b700e82db81d8468d4f332b4413f3cc90da55675
Clement Perier Muzet <barceloneta08003@gmail.com>
2020-10-24 18:17:08 +0200

    back end completed

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,37 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params_cocktails)
+    @cocktail.save
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+
+    redirect_to cocktails_path
+  end
+
+    private
+
+  def params_cocktails
+    params.require(:cocktail).permit(:name)
+  end
+end
