b9551f99ad7f920230daf2a2b3d6300af5912e6d
Clement Perier Muzet <barceloneta08003@gmail.com>
2020-10-28 22:21:25 +0100

    before typed.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,13 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
+import { typed } from '../plugins/typed';
+
+typed();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// const imagePath = (name) => images(name, true) 
---
+++ app/javascript/plugins/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+const options = {
+  stringsElement: '#typed-strings',
+  typeSpeed: 40,
+  loop: true
+};
+
+const typed = new Typed('#typed-strings', options);
+
+export { typed };
