0254894bce5d309df22139406dedde0b434fd1e9
Clement Perier Muzet <barceloneta08003@gmail.com>
2020-11-02 22:35:43 +0100

    splash page ongoing

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const typed = () => {
   new Typed('#typed-strings', { 
-    strings: ['One cocktail is alright','two is too many', 'three is not enough'], 
+    strings: ['One cocktail is alright','Two is too many', 'Three is not enough'], 
   typeSpeed: 70, 
   loop:true
 });
