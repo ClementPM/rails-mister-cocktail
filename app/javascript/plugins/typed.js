7b2bc8dc69d429a320830a461ded61b20eec7833
Clement Perier Muzet <barceloneta08003@gmail.com>
2020-11-02 23:30:33 +0100

    main banner & navbar

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const typed = () => {
   new Typed('#typed-strings', { 
     strings: ['One cocktail is alright','Two is too many', 'Three is not enough'], 
-  typeSpeed: 70, 
+  typeSpeed: 120, 
   loop:true
 });
 }
